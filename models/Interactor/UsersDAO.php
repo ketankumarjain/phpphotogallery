6fa3c3b6733f1895e48077639a63d556c14c0b0b
ketan <ketan.jain@live.com>
2015-06-01 18:20:50 +0530

    refactor magic string into Constant

--- models/Interactor/UsersDAO.php
+++ models/Interactor/UsersDAO.php
@@ -11,15 +11,21 @@ use Exception;
 require_once dirname(__DIR__)."/../config/Auto_load.php";
 class UsersDAO extends DAO {
 
+    const tablename = "galleryusers";
+
+    const userNameKey = "username";
+
+    const passwordkey = "password";
+
     public function __construct(DBGetway $db){
        $this->db=$db;
-        $this->table_name="galleryusers";
+        $this->table_name= self::tablename;
     }
     public  function authenticate($user, $pass) {
         $username = $user;
         $password =$pass;
         //code smell
-        $results = $this->db->find_by_Values("username",$username,"password",$password,$this->table_name);
+        $results = $this->db->find_by_Values(self::userNameKey,$username, self::passwordkey,$password,$this->table_name);
         if($results==null)
             throw new Exception("No Content Found");
         return $results;
